acb069b99a32599e02d035b2f20b694feeb45fdf
Jennie <cassiopeia5571@gmail.com>
2020-07-26 18:10:02 -0500

    tutorial thru step #145

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Burger.module.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+   //**check out default javascript array functions (i.e. map & reduce)
+  .map((ingrKey) => {
+      return [...Array(props.ingredients[ingrKey])].map((_, index) => {
+        return <BurgerIngredient key={ingrKey + index} type={ingrKey} />;
+      });
+    })
+    .reduce((reducedValueArray, currentValue) => {
+      return reducedValueArray.concat(currentValue);
+   //below is either an empty array or contains jsx elements
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar"
 
 const layout = props => (
     <Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>{props.children}</main>
+        <Toolbar />
+        <main className={classes.Content}>
+            {props.children}
+            </main>
     </Fragment>
 );
 
