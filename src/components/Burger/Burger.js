37d2debe8357e9ca05266d3fcd07dd007879c1e3
Jennie <cassiopeia5571@gmail.com>
2020-08-01 19:20:25 -0500

    section 12 - passing data on

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import {withRouter} from 'react-router-dom'
 
 const burger = (props) => {
+  console.log(props);
   let transformedIngredients = Object.keys(props.ingredients)
    //**check out default javascript array functions (i.e. map & reduce)
   .map((ingrKey) => {
@@ -26,4 +28,4 @@ const burger = (props) => {
   );
 };
 
-export default burger;
+export default withRouter(burger);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {BrowserRouter as Router} from 'react-router-dom';
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+ const app = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode> 
+     );
+   ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
